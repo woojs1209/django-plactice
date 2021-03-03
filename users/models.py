c5a3e813a074734b3ddc80c35d1d868b47ba818c
woojs1209 <76648638+woojs1209@users.noreply.github.com>
2021-03-03 17:27:20 +0900

    users models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+    
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other")
+    )
+
+    LANGAUGE_E = "en"
+    LANGAUGE_K = "kr"
+
+    LANGAUGE_CHOICES = (
+        (LANGAUGE_E, "English"),
+        (LANGAUGE_K, "Korean")
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW")
+    )
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices = GENDER_CHOICES, max_length=10, blank=True)
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    langauge = models.CharField(choices=LANGAUGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    superhost= models.BooleanField(default=False)
